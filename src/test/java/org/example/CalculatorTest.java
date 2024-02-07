7e2ff16562af24500594a373fb3f790999864df7
Adam Kaniecki <adam2113@poczta.onet.pl>
2024-02-07 15:49:30 +0100

    przykładowy test, zgodny z oczekiwaniami

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +9,15 @@ class CalculatorTest {
 
     @Test
     void add() {
+//    given
+        int left = 5;
+        int right = 3;
+        Integer expected = 8;
+
+//    when
+        Integer result = Calculator.add(left, right);
+
+//    then
+        Assertions.assertEquals(expected,result);
     }
 }
